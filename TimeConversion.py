fd92dbbabd3f9e793929678e4c60a9f27b91d150
Raul T <jr.torsa@gmail.com>
2020-04-09 16:04:39 -0600

    Finished Time Conversion exercise

--- TimeConversion.py
+++ TimeConversion.py
@@ -1,13 +1,31 @@
-# s = input('Time Conversion: ')
-s = '07:05:45PM'
+# Python program to convert time 
+# from 12 hour to 24 hour format 
+  
+# Function to convert the date format 
+def convert24(str1): 
+      
+    # Checking if last two elements of time 
+    # is AM and first two elements are 12 
+    if str1[-2:] == "AM" and str1[:2] == "12": 
+        return "00" + str1[2:-2] 
+          
+    # remove the AM     
+    elif str1[-2:] == "AM": 
+        return str1[:-2] 
+      
+    # Checking if last two elements of time 
+    # is PM and first two elements are 12    
+    elif str1[-2:] == "PM" and str1[:2] == "12": 
+        return str1[:-2] 
+          
+    else: 
+          
+        # add 12 to hours and remove PM 
+        return str(int(str1[:2]) + 12) + str1[2:8] 
+  
+# Driver Code         
+print(convert24("09:05:45 PM")) 
 
-AM = ['00','01','02','03','04','05','06','07','08','09','10','11']
-PM = ['12','13','14','15','16','17','18','19','20','21','22','23']
-
-if 'PM' in s:
-    idx = AM.index(s[:2])
-    print(idx)
-                     
            
         
     
